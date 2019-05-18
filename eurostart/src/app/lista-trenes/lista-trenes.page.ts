53c3ff45dccb04c2429d69e5119881602adb935a
Pochu <pochuelo15@gmail.com>
2019-05-18 13:02:16 +0200

    lista trenes
    No tiene ngfor porque de momento solo hay un bucle, en el caso de no recibir datos redirige a la página principal

--- eurostart/src/app/lista-trenes/lista-trenes.page.ts
+++ eurostart/src/app/lista-trenes/lista-trenes.page.ts
@@ -1,26 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiResponse } from '../api-response';
+import { Storage } from '@ionic/storage';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lista-trenes',
   templateUrl: './lista-trenes.page.html',
   styleUrls: ['./lista-trenes.page.scss'],
 })
+
 export class ListaTrenesPage implements OnInit {
 
-  data: {
-    destino: String,
-    origen: String,
-    personas: Number,
-    precio: Number
-  }
+  data:ApiResponse
+  hasLoaded:Boolean = false
 
-  constructor() { }
+  constructor(private storage: Storage, private router: Router) { }
 
   loadData() {
-    
+    this.storage.get('TRAVEL').then((viaje: ApiResponse) => {
+      if (viaje != null) {
+        this.data = viaje;
+        this.hasLoaded = true;
+      }
+      else this.router.navigateByUrl('')
+    })
   }
 
   ngOnInit() {
+    this.loadData();
   }
 
 }
