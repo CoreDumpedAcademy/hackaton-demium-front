7d39908ee8548504a51d8a15f0825797c24571fc
Pochu <pochuelo15@gmail.com>
2019-05-18 16:20:39 +0200

    display de redes sociales

---
+++ eurostart/src/app/listahoteles/listahoteles.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listahoteles',
+  templateUrl: './listahoteles.page.html',
+  styleUrls: ['./listahoteles.page.scss'],
+})
+export class ListahotelesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
