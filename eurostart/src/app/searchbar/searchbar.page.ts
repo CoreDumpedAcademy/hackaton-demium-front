2119e1ea8728bd00c181fb3aedb8314cf5e1b92c
w0xter <lpozogilo@gmail.com>
2019-05-18 14:40:44 +0200

    search train

--- eurostart/src/app/searchbar/searchbar.page.ts
+++ eurostart/src/app/searchbar/searchbar.page.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.page.html',
   styleUrls: ['./searchbar.page.scss'],
 })
 export class SearchbarPage implements OnInit {
-
+  citiesList= ['Madrid',
+    'Barcelona',
+    'Málaga',
+    'Granada',
+    'Pamplona']
+  fromDate:string= new Date().toISOString();
+  toDate:string;
+  numberPassengers:number = 0;
   constructor() { }
+  addPassenger(){
+    this.numberPassengers++;
+  }
+  subPassenger(){
+    if(this.numberPassengers > 0) this.numberPassengers = this.numberPassengers - 1
 
+  }
   ngOnInit() {
+    this.fromDate="";
+
   }
 
 }
