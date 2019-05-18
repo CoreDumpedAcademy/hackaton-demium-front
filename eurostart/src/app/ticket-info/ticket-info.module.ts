dca11264fddc4c16f6576a66d7834f5afe04d01f
TebionDL <purreren@gmail.com>
2019-05-18 14:20:22 +0200

    added html to ticket-info

--- eurostart/src/app/ticket-info/ticket-info.module.ts
+++ eurostart/src/app/ticket-info/ticket-info.module.ts
@@ -23,4 +23,5 @@ const routes: Routes = [
   ],
   declarations: [TicketInfoPage]
 })
-export class TicketInfoPageModule {}
+export class TicketInfoPageModule {
+}
--- eurostart/src/app/ticket-info/ticket-info.page.ts
+++ eurostart/src/app/ticket-info/ticket-info.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-ticket-info',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketInfoPage implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  name: String;
+  lastName: String;
+  start: String;
+  finish: String;
+  price: number;
+  pricep: number;
+  people: number;
 
   ngOnInit() {
+    this.http.get('http://localhost:3000/ticket/'+this.lastName)
+    this.pricep = this.price/this.people;
   }
 
 }
